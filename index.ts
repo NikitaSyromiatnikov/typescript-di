87ed5d6722e5857a90f94397562ac295b0588ada
Nikita Syromiatnikov <nikitasyromiatnikovdev@gmail.com>
2022-07-05 13:43:56 +0300

    Make public package

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { Container } from "./lib/container/Container";
+import { IContainer } from "./lib/container/IContainer";
+import { IModifiable } from "./lib/container/IModifiable";
+import { ResolutionStrategy } from "./lib/container/ResolutionStrategy";
+import { Injectable } from "./lib/decorators";
+
+export { Container, IContainer, IModifiable, ResolutionStrategy, Injectable };
